31adf146f5723dc3f4ec19e7d03e001a347dcd41
An4bo1ik <shura.shapovalov.19@mail.ru>
2023-11-09 14:59:15 +0700

    commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,15 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      Car car1 = new Car("Porsche", 911);
+      Car car2 = new Car("Mazda", 3);
+      Car car3 = new Car("Lexus", 650);
+      Car car4 = new Car("Nissan", 350);
+
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", car1));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", car2));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", car3));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", car4));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -28,6 +34,11 @@ public class MainApp {
          System.out.println("Email = "+user.getEmail());
          System.out.println();
       }
+
+      userService.getByCar("Porsche", 911);
+      userService.getByCar("Mazda", 3);
+      userService.getByCar("Lexus", 650);
+      userService.getByCar("Nissan", 350);
 
       context.close();
    }
--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -26,4 +26,14 @@ public class UserDaoImp implements UserDao {
       return query.getResultList();
    }
 
+   @Override
+   @SuppressWarnings("uncheked")
+   public User getByCar(String model, int series) {
+      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User user" +
+                      " where user.car.model = :model and user.car.series = :series")
+              .setParameter("model", model)
+              .setParameter("series", series);
+      return query.getResultList().size() > 0 ? query.getResultList().get(0) : null;
+   }
+
 }
